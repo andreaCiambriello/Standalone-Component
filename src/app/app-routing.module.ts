d344e62a3f01bc0dad92be5f70ff7d6fae6d379e
Andrea <andrea.ciambriello@hotmail.com>
2023-12-23 17:23:51 +0100

    Standalone component blueprint

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { WelcomeComponent } from "./welcome/welcome.component";
+import { AboutComponent } from "./about/about.component";
+
+const routes: Routes = [
+    {
+        path: '',
+        component: WelcomeComponent
+    },
+    {
+        path: 'about',
+        // component: AboutComponent
+        loadComponent: () => 
+        import('./about/about.component').then((mod) => mod.AboutComponent)
+    },
+    {
+        path: 'dashboard',
+        loadChildren: () => 
+            import('./dashboard/routes').then(
+                (mod) => mod.DASHBOARD_ROUTES
+            ),
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule{}
